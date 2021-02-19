af5676f6806d0dcbed7a0e2ca21aba7a9d1df984
milanochuang <milanochuang@gmai.com>
2021-02-19 16:23:15 +0800

    Adjustment

--- milano_bot.py
+++ milano_bot.py
@@ -17,20 +17,7 @@ BOT_NAME = "幫你買票機器人"
 
 client = discord.Client()
 
-stationDICT=[
-            {'stationName': '南港', 'stationID': '0990', 'stationSeq': 1},
-            {'stationName': '台北', 'stationID': '1000', 'stationSeq': 2},
-            {'stationName': '板橋', 'stationID': '1010', 'stationSeq': 3},
-            {'stationName': '桃園', 'stationID': '1020', 'stationSeq': 4},
-            {'stationName': '新竹', 'stationID': '1030', 'stationSeq': 5},
-            {'stationName': '苗栗', 'stationID': '1035', 'stationSeq': 6},
-            {'stationName': '台中', 'stationID': '1040', 'stationSeq': 7},
-            {'stationName': '彰化', 'stationID': '1043', 'stationSeq': 8},
-            {'stationName': '雲林', 'stationID': '1047', 'stationSeq': 9},
-            {'stationName': '嘉義', 'stationID': '1050', 'stationSeq': 10},
-            {'stationName': '台南', 'stationID': '1060', 'stationSeq': 11},
-            {'stationName': '左營', 'stationID': '1070', 'stationSeq': 12},
-            ]
+
 
 def amountSTRConvert(inputSTR):
     resultDICT={}
