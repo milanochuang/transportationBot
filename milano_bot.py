86aa09b8a8ceb7c0fbcbf1b2da6bd491e45c2595
milanochuang <milanochuang@gmai.com>
2021-02-19 14:09:24 +0800

    Adjustments

--- milano_bot.py
+++ milano_bot.py
@@ -5,7 +5,7 @@ import discord
 from TransportationBot import runLoki
 import json
 
-DISCORD_TOKEN="Nzg5Mzc0ODk3OTA5Mzk5NjA1.X9xIqQ.nz80BzVlbyUTBm-2hVxfuRCnOTI"
+DISCORD_TOKEN=""
 DISCORD_GUILD="Droidtown Linguistics Tech."
 BOT_NAME = "幫你買票機器人"
 
